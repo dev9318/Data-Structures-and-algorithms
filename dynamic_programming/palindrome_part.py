80185732995ed9146b7a30f7eee34fb65462397c
dev9318 <dev.moxaj@gmail.com>
2020-10-13 12:09:05 +0530

    Added O(n^3)

---
+++ dynamic_programming/palindrome_part.py
@@ -0,0 +1,21 @@
+# Min no of cuts required to partition a string into 
+# palindromes
+
+# O(n^3) approach
+def ispalin(st):
+    return st == st[::-1]
+
+def palindrome_count(st,i,j):
+    if (i>=j or ispalin(st[i,j+1])):
+        return 0
+
+    ans = 1000000000000
+    for k in range(i,j):
+        count = (1 + palindrome_count(st,i,k) + 
+        palindrome_count(st,k+1,j))
+        ans = min(count,ans)
+    print(ans)
+
+if __name__ == "__main__":
+    string = "ababbbabbababa"
+    palindrome_count(string,0,len(string)-1)
