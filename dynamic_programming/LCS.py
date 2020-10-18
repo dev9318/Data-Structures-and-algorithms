7a9c35157999fa4451dd327d6086eac54d5c8170
dev9318 <dev.moxaj@gmail.com>
2020-10-18 14:36:42 +0530

    added dp-approach

--- dynamic_programming/LCS.py
+++ dynamic_programming/LCS.py
@@ -10,6 +10,21 @@ def LCS(a,b,m,n):
     else:
         return max(LCS(a,b,m,n-1),LCS(a,b,m-1,n))
 
+# DP approach
+
+def lcs(a,b):
+    m = len(a)
+    n = len(b)
+    C = [[None]*(n+1) for _ in range(m+1)]
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                C[i][j] = 0
+            elif a[i-1] == b[j-1]:
+                C[i][j] = 1 + C[i-1][j-1]
+            else:
+                C[i][j] = max(C[i-1][j],C[i][j-1])
+    return C[m][n]
 
 if __name__ == "__main__":
-    print(LCS("AGGTAB","GXTXAYB",6,7))
+    print(lcs("AGGTAB","GXTXAYB"))
