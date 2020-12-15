9a74e157ebd7993bc035e8f3254f23705379ce2e
dev9318 <dev.moxaj@gmail.com>
2020-12-15 23:16:04 +0530

    Update z-algorithm.py

--- string-manipulations/z-algorithm.py
+++ string-manipulations/z-algorithm.py
@@ -4,5 +4,22 @@ def zarray(pattern, string):
     string = pattern + '$' + string
     z = [0]*len(string)
     l = r = k = 0
+    n = len(string)
     for i in range(1,n):
-        
+        if (i > r): 
+            l = r = i
+            # "aaaaaa" and i = 1, Z[i] and R become 5 
+            while r < n and string[r - l] == string[r]: 
+                r += 1
+            z[i] = r - l 
+            r -= 1
+        else: 
+            k = i - l 
+            if (z[k] < r - i + 1): 
+                z[i] = z[k] 
+            else: 
+                l = i 
+                while (r < n and string[r - l] == string[r]): 
+                    r += 1
+                z[i] = r - l 
+                r -= 1
